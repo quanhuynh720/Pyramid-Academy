b527442a4b16a5c29344dd9c95f752185557584e
quan.huynh <quan.huynh720@outlook.com>
2022-02-20 22:49:53 -0600

    fix combat

--- globinvshuman/src/java/Combat.java
+++ globinvshuman/src/java/Combat.java
@@ -4,10 +4,10 @@ public class Combat {
 
     public static void fight() {
         int damage = (int) (Math.random() * 9) + 1;
-        int humanHealth = human.getHealth();
-        human.setHealth(humanHealth - damage);
 
-    while(goblin.getHealth() >=0 ) {
+    for(int i = 0; i <= goblin.getHealth(); i++) {
+        human.setHealth(human.getHealth() - damage);
+        goblin.setHealth(goblin.getHealth() - damage);
         if (human.getHealth() <= 0) {
             System.out.print("\n Your human have died in combat!");
         } else if (goblin.getHealth() <=0 ){
@@ -15,7 +15,7 @@ public class Combat {
             Human.receiveLoot();
         } else {
             System.out.print("\n You have taken " + damage + " damage." +
-                    "\n You now have " + humanHealth + " HP left.");
+                    "\n You now have " + human.getHealth() + " HP left.");
         }
     }
     }
